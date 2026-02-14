5387efcaf83b5d76ce65e22d66a7b53baccd4012
Rampathak12 <162395208+Rampathak12@users.noreply.github.com>
2026-02-14 08:23:17 +0545

    Update theme.kt

--- theme.kt
+++ theme.kt
@@ -1,20 +1,29 @@
+package com.example.smarttipmanager.ui.theme
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
 @Composable
 fun SmartTipTheme(
     darkTheme: Boolean,
     content: @Composable () -> Unit
 ) {
 
-    val colors = if (darkTheme) darkColorScheme(
-        primary = Color(0xFF81C784),
-        background = Color(0xFF121212)
-    ) else lightColorScheme(
-        primary = Color(0xFF4CAF50),
-        background = Color(0xFFF5F5F5)
-    )
+    val colors = if (darkTheme) {
+        darkColorScheme(
+            primary = Color(0xFF81C784),
+            background = Color(0xFF121212)
+        )
+    } else {
+        lightColorScheme(
+            primary = Color(0xFF4CAF50),
+            background = Color(0xFFF5F5F5)
+        )
+    }
 
     MaterialTheme(
         colorScheme = colors,
-        typography = Typography,
         content = content
     )
 }
